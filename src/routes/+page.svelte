4be3a14c3cc58195f1ba3540231a02f57876f3be
Tobi <tobi@nuetzliche.it>
2023-10-18 08:22:42 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { PUBLIC_API } from "$env/static/public";
+
+console.log(PUBLIC_API);
+
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+
+<p>You are running in "{PUBLIC_API}" mode.</p>
